bc89cb451a126ce3767ca805871bdd846cab63a1
sravyamyla <sm465@njit.edu>
2021-12-14 20:27:37 -0500

    Commit

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,15 +1,15 @@
-"""Importing Calculator Class from calculator > main.py for Testing"""
+"""Importing Calculator main.py for Testing"""
 import pytest
 from calculator.main import Calculator
 from calculator.history_calculations.history_calculations import History
 
 # pylint: disable=unused-argument,redefined-outer-name
 
-num1, num2, add, sub, multi, div = Calculator('csv_handling/input/data.csv').get_data()
+num_1, num_2, add, sub, multi, div = Calculator('csv_handling/input/data.csv').get_data()
 length = len(add)
 
 
-# This is called a fixture and it runs each time you pass it to a test
+# This is a fixture
 @pytest.fixture
 def clear_history():
     """ Clears history """
@@ -20,7 +20,7 @@ def test_calculator_add(clear_history):
     """ To check if calculator addition result is correct """
 
     for i in range(length):
-        assert Calculator.add_nums(num1[i], num2[i]) == add[i]
+        assert Calculator.add_nums(num_1[i], num_2[i]) == add[i]
     assert History.get_calculation_count() == 6
     assert History.get_last_calculation_added() == add[-1]
     assert History.get_first_calculation_history() == add[0]
@@ -29,16 +29,16 @@ def test_calculator_add(clear_history):
 def test_calculator_subtract(clear_history):
     """ To check if calculator subtraction result is correct """
     for i in range(length):
-        assert Calculator.subtract_nums(num1[i], num2[i]) == sub[i]
+        assert Calculator.subtract_nums(num_1[i], num_2[i]) == sub[i]
 
 
 def test_calculator_multiply(clear_history):
     """ To check if calculator multiplication result is correct """
     for i in range(length):
-        assert Calculator.multiply_nums(num1[i], num2[i]) == multi[i]
+        assert Calculator.multiply_nums(num_1[i], num_2[i]) == multi[i]
 
 
 def test_calculator_divide(clear_history):
     """ To check if calculator division result is correct """
     for i in range(length):
-        assert Calculator.divide_nums(num1[i], num2[i]) == div[i]
+        assert Calculator.divide_nums(num_1[i], num_2[i]) == div[i]
